e6675418e8d798389182c7fecfc2d28354a5f95f
faberh12 <hernandezfabian828@gmail.com>
2024-05-26 17:50:03 -0500

    Actualizacion del archivo comment.py en cada carpeta

--- src/models/qualification.py
+++ src/models/qualification.py
@@ -1,3 +1,17 @@
 from fastapi import APIRouter, Body, Query, Path, status, Depends
 from fastapi.responses import JSONResponse
 from typing import List, Annotated
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from src.config.database import Base
+
+class Qualification(Base):
+    __tablename__ = "qualifications"
+
+    id = Column(Integer, primary_key=True, index=True)
+    score = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    city_id = Column(Integer, ForeignKey('cities.id'))
+
+    user = relationship("User", back_populates="qualifications")
+    city = relationship("City", back_populates="qualifications")
